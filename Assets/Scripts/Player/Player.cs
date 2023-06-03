ca4b0a3a099de9252fff424565b6f19d217cc767
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-06-03 16:14:09 +0900

    플레이어 코드 수정

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,18 +17,34 @@ public class Player : MonoBehaviour
         anim = GetComponent<Animator>();
        realMaxSpeed = maxSpeed;
     }
-    void Update()
+    private void FixedUpdate()
     {
-        //Jump
-        //stop speed
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
-            realMaxSpeed = maxSpeed;
-            anim.SetBool("IsRun", false);
-        }
-        if (Input.GetButtonDown("Horizontal"))
-            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        Move(); //플레이어 이동
+        JumpCheck(); //플레이어 바닥에 닿았을 때 점프 가능한지 체크
+        Turn(); //이미지 좌우전환
+        Run(); //달리기 
+        Jump(); //점프
+    }
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //플레이어 이동 속도 제어
+        if (rigid.velocity.x > realMaxSpeed)
+            rigid.velocity = new Vector2(realMaxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < realMaxSpeed * (-1))
+            rigid.velocity = new Vector2(realMaxSpeed * (-1), rigid.velocity.y);
+
+        //걷기 애니메이션
+        if (Mathf.Abs(rigid.velocity.x) < 0.3) 
+            anim.SetBool("IsWalk", false);
+        else
+            anim.SetBool("IsWalk", true);
+    }
+    private void Run()
+    {
+        //쉬프트 눌렀을 때 달리기 아니면 걷기
         if (Input.GetKey(KeyCode.LeftShift))
         {
             realMaxSpeed = 10f;
@@ -39,30 +55,10 @@ public class Player : MonoBehaviour
             realMaxSpeed = maxSpeed;
             anim.SetBool("IsRun", false);
         }
-        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJump"))
-        {
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            anim.SetBool("IsJump", true);
-            anim.SetBool("IsRun", false);
-        }
-        //Animation
-        if (Mathf.Abs(rigid.velocity.x) < 0.3)
-            anim.SetBool("IsWalk", false);
-        else
-            anim.SetBool("IsWalk", true);
     }
-    private void FixedUpdate()
+    private void JumpCheck()
     {
-        //Move by Key Control
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-         
-        if (rigid.velocity.x > realMaxSpeed)
-            rigid.velocity = new Vector2(realMaxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < realMaxSpeed * (-1))
-            rigid.velocity = new Vector2(realMaxSpeed*(-1), rigid.velocity.y);
-
-        if(rigid.velocity.y < 0)
+        if (rigid.velocity.y < 0)
         {
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
@@ -72,5 +68,19 @@ public class Player : MonoBehaviour
                     anim.SetBool("IsJump", false);
             }
         }
+    }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJump")) //점프 가능한 상태일 때 점프 기능
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("IsJump", true);
+            anim.SetBool("IsRun", false);
+        }
+    }
+    private void Turn()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
     }
 }
