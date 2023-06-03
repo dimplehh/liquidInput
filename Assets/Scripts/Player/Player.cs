f1229116bb0fbc3629f0a0e8683c77818d6d9dd9
HyunHee_LEE <dimplehh@naver.com>
2023-06-03 16:48:38 +0900

    jump 고침 .. 아마도?

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -64,6 +64,7 @@ public class Player : MonoBehaviour
     {
         if (rigid.velocity.y < 0)
         {
+            Debug.Log("check2");
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
             if (rayHit.collider != null)
@@ -71,6 +72,10 @@ public class Player : MonoBehaviour
                 if (rayHit.distance < 0.5f)
                     anim.SetBool("IsJump", false);
             }
+        }
+        else
+        {
+            anim.SetBool("IsJump", false);
         }
     }
     private void Jump()
