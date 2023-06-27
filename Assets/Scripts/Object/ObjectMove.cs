920b861a6085da170aad4290803ac80ceb59b2b7
HyunHee_LEE <dimplehh@naver.com>
2023-06-27 19:12:49 +0900

    push and pull onCollisionStay로 우선 고침, 오류 있을 수도 ㅇ

--- Assets/Scripts/Object/ObjectMove.cs
+++ Assets/Scripts/Object/ObjectMove.cs
@@ -5,36 +5,33 @@ using UnityEngine;
 public class ObjectMove : MonoBehaviour
 {
     public GameObject player;
-    private void Update()
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
-             if(player && player.GetComponent<Player>().canGrab)
-            {        //4가지 경우. 1.player가 object보다 왼쪽에 있고 왼쪽으로 갈 때, 2.player가 object보다 왼쪽에 있고 오른쪽으로 갈 때, 3.player가 object보다 오른쪽에 있고 왼쪽으로 갈 때, 4.player가 object보다 오른쪽에 있고 오른쪽으로 갈 때
-            Debug.Log("if문 들어옴");
+        if (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<Player>().canGrab)
+        {
+            player = collision.gameObject;
             if ((player.GetComponent<Player>().h < 0 && player.transform.position.x < this.transform.position.x))
-                {
-                    player.GetComponent<Player>().anim.SetBool("isPull", true);
-                    player.GetComponent<Player>().spriteRenderer.flipX = false;
+            {
+                player.GetComponent<Player>().anim.SetBool("isPull", true);
+                player.GetComponent<Player>().spriteRenderer.flipX = false;
+                this.transform.position = new Vector3(this.transform.position.x - 0.02f, this.transform.position.y, this.transform.position.z);
+            }
+            else if (player.GetComponent<Player>().h < 0 && player.transform.position.x > this.transform.position.x)
+            {
+                player.GetComponent<Player>().anim.SetBool("isPush", true);
                 this.transform.position = new Vector3(this.transform.position.x - 0.01f, this.transform.position.y, this.transform.position.z);
-                Debug.Log(this.transform.position);
-                }
-                else if (player.GetComponent<Player>().h < 0 && player.transform.position.x > this.transform.position.x)
-                {
-                    player.GetComponent<Player>().anim.SetBool("isPush", true);
-                    this.transform.position = new Vector3(this.transform.position.x - 0.005f, this.transform.position.y, this.transform.position.z);
-                Debug.Log(this.transform.position);
             }
-                else if ((player.GetComponent<Player>().h > 0 && player.transform.position.x < this.transform.position.x))
-                {
-                    player.GetComponent<Player>().anim.SetBool("isPush", true);
-                    this.transform.position = new Vector3(this.transform.position.x + 0.005f, this.transform.position.y, this.transform.position.z);
-                Debug.Log(this.transform.position);
+            else if ((player.GetComponent<Player>().h > 0 && player.transform.position.x < this.transform.position.x))
+            {
+                player.GetComponent<Player>().anim.SetBool("isPush", true);
+                this.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y, this.transform.position.z);
             }
-                else if (player.GetComponent<Player>().h > 0 && player.transform.position.x > this.transform.position.x)
-                {
-                    player.GetComponent<Player>().anim.SetBool("isPull", true);
-                    player.GetComponent<Player>().spriteRenderer.flipX = true;
-                    this.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y, this.transform.position.z);
-                Debug.Log(this.transform.position);
+            else if (player.GetComponent<Player>().h > 0 && player.transform.position.x > this.transform.position.x)
+            {
+                player.GetComponent<Player>().anim.SetBool("isPull", true);
+                player.GetComponent<Player>().spriteRenderer.flipX = true;
+                this.transform.position = new Vector3(this.transform.position.x + 0.02f, this.transform.position.y, this.transform.position.z);
             }
         }
     }
