972acc7ff700170218bc166bf556c127ca8a64b7
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-07-02 15:52:58 +0900

    savezone 코드 수정

--- Assets/Scripts/SaveZone.cs
+++ Assets/Scripts/SaveZone.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class SaveZone : MonoBehaviour
 {
     private int index;
-    private int count; 
+    private bool isSave = false; 
     private void Start()
     {
         index = Managers.Data.playerData.index;
-        count = 0;
+        isSave = true;
     }
     private void OnTriggerStay2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("SaveZone")&& Input.GetKey(KeyCode.X) && count == 0) //player 안에 자식오브젝트로 saveZone이라는 태그를 가진 충돌체 생성
+        if(other.gameObject.CompareTag("SaveZone")&& Input.GetKey(KeyCode.X) && isSave) //player 안에 자식오브젝트로 saveZone이라는 태그를 가진 충돌체 생성
         {
             Managers.Data.SaveData(index, other.gameObject, GameManager.instance.currentStage+1, GameManager.instance.curWaterReserves);
             Debug.Log(index + " 에 저장하였습니다.");
             index++;
-            count++;
+            isSave = false;
             if(index == 4)
             {
                 index = 1;
@@ -29,7 +29,7 @@ public class SaveZone : MonoBehaviour
     {
         if (other.gameObject.CompareTag("SaveZone"))
         {
-            count--;
+            isSave = true;
             Debug.Log("player가 SaveZone에서 나갔습니다.");
         }
     }
