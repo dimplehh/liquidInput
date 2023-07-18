6dfc5ddb8bf4b530a303e661c8a4e7fde7cb0a6e
HyunHee_LEE <dimplehh@naver.com>
2023-07-18 16:45:10 +0900

    구역 3 1/2 + 구역 5 1/2 배치

--- Assets/Scripts/Object/ObjectMove.cs
+++ Assets/Scripts/Object/ObjectMove.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ObjectMove : MonoBehaviour
 {
     public GameObject player;
-
+    [SerializeField]
+    float pullSpeed = 0.02f;
+    [SerializeField]
+    float pushSpeed = 0.01f;
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<Player>().canGrab)
@@ -15,23 +18,23 @@ public class ObjectMove : MonoBehaviour
             {
                 player.GetComponent<Player>().anim.SetBool("isPull", true);
                 player.GetComponent<Player>().spriteRenderer.flipX = false;
-                this.transform.position = new Vector3(this.transform.position.x - 0.02f, this.transform.position.y, this.transform.position.z);
+                this.transform.position = new Vector3(this.transform.position.x - pullSpeed, this.transform.position.y, this.transform.position.z);
             }
             else if (player.GetComponent<Player>().h < 0 && player.transform.position.x > this.transform.position.x)
             {
                 player.GetComponent<Player>().anim.SetBool("isPush", true);
-                this.transform.position = new Vector3(this.transform.position.x - 0.01f, this.transform.position.y, this.transform.position.z);
+                this.transform.position = new Vector3(this.transform.position.x - pushSpeed, this.transform.position.y, this.transform.position.z);
             }
             else if ((player.GetComponent<Player>().h > 0 && player.transform.position.x < this.transform.position.x))
             {
                 player.GetComponent<Player>().anim.SetBool("isPush", true);
-                this.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y, this.transform.position.z);
+                this.transform.position = new Vector3(this.transform.position.x + pushSpeed, this.transform.position.y, this.transform.position.z);
             }
             else if (player.GetComponent<Player>().h > 0 && player.transform.position.x > this.transform.position.x)
             {
                 player.GetComponent<Player>().anim.SetBool("isPull", true);
                 player.GetComponent<Player>().spriteRenderer.flipX = true;
-                this.transform.position = new Vector3(this.transform.position.x + 0.02f, this.transform.position.y, this.transform.position.z);
+                this.transform.position = new Vector3(this.transform.position.x + pullSpeed, this.transform.position.y, this.transform.position.z);
             }
         }
     }
