6af9b9e2e4e8828fbd7559e284422eb5b4cd5232
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-08-03 23:32:47 +0900

    키보드입력 인덱스 위치 이미지 추가

--- Assets/Resources/Scripts/Save/KeyBoardClick/ChapterLoadKeyBoardClick.cs
+++ Assets/Resources/Scripts/Save/KeyBoardClick/ChapterLoadKeyBoardClick.cs
@@ -6,9 +6,13 @@ public class ChapterLoadKeyBoardClick : MonoBehaviour
 {
     [SerializeField] private ChapterLoad chapterLoad; //챕터별 불러오기
     [SerializeField] private int chapLoadindex = 1; //챕터별 인덱스
+    [SerializeField] private RectTransform selectCheckImage; //선택 확인 이미지
+    [SerializeField] private RectTransform[] selectPos; //선택이미지 위치
+    
     private void OnEnable()
     {
         chapLoadindex = 1;
+        selectCheckImage.position = selectPos[0].position;
     }
     private void Update()
     {
@@ -19,13 +23,30 @@ public class ChapterLoadKeyBoardClick : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            chapLoadindex++;
-            if (chapLoadindex >= 4) chapLoadindex = 4;
+            if (chapLoadindex >= 4)
+            {
+                chapLoadindex = 4;
+                selectCheckImage.position = selectPos[3].position;
+            }
+            else
+            {
+                chapLoadindex++;
+                selectCheckImage.position = selectPos[chapLoadindex - 1].position;
+            }
+  
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            chapLoadindex--;
-            if (chapLoadindex <= 1) chapLoadindex = 1;
+            if (chapLoadindex <= 1)
+            {
+                chapLoadindex = 1;
+                selectCheckImage.position = selectPos[0].position;
+            }
+            else
+            {
+                chapLoadindex--;
+                selectCheckImage.position = selectPos[chapLoadindex - 1].position;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
--- Assets/Resources/Scripts/Save/KeyBoardClick/LoadKeyBoardClick.cs
+++ Assets/Resources/Scripts/Save/KeyBoardClick/LoadKeyBoardClick.cs
@@ -6,9 +6,12 @@ public class LoadKeyBoardClick : MonoBehaviour
 {
     [SerializeField] private int loadindex; //챕터는 1부터 시작이고 이거는 0부터 시작하는거 통일성있게 가야함
     [SerializeField] private LoadSlotSelect loadSlotSelect; //수동 불러오기
+    [SerializeField] private RectTransform selectCheckImage; //선택 확인 이미지
+    [SerializeField] private RectTransform[] selectPos; //선택이미지 위치
     private void OnEnable()
     {
         loadindex = 0;
+        selectCheckImage.position = selectPos[0].position;
     }
     private void Update()
     {
@@ -19,13 +22,29 @@ public class LoadKeyBoardClick : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            loadindex++;
-            if (loadindex >= 3) loadindex = 3;
+            if (loadindex >= 3)
+            {
+                loadindex = 3;
+                selectCheckImage.position = selectPos[3].position;
+            }
+            else
+            {
+                loadindex++;
+                selectCheckImage.position = selectPos[loadindex].position;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            loadindex--;
-            if (loadindex <= 0) loadindex = 0;
+            if (loadindex <= 0)
+            {
+                loadindex = 0;
+                selectCheckImage.position = selectPos[0].position;
+            }
+            else
+            {
+                loadindex--;
+                selectCheckImage.position = selectPos[loadindex].position;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
