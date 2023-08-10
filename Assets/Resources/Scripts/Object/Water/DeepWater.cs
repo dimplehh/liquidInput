64815f02f12f24c06dd843ab4529a7c116c7fe3e
HyunHee_LEE <dimplehh@naver.com>
2023-08-10 14:15:03 +0900

    깊은물 2차

--- Assets/Resources/Scripts/Object/Water/DeepWater.cs
+++ Assets/Resources/Scripts/Object/Water/DeepWater.cs
@@ -20,15 +20,29 @@ public class DeepWater : Water
     //protected virtual void LateUpdate()
     //{
     //    currentWaterReservesImage.fillAmount = Mathf.Lerp(currentWaterReservesImage.fillAmount, (float)currentWaterReserves / (float)maxWaterReserves / 1 / 1, Time.deltaTime * 5f);
-
     //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!GameManager.instance.player.GetComponent<Player>().isSlime) //슬라임 형태가 아닐때 
+        {
+            GameManager.instance.player.GetComponent<Rigidbody2D>().mass = 1.5f;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameManager.instance.player.GetComponent<Rigidbody2D>().mass = 0.6f;
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("SaveZone"))
         {
-            if (!GameManager.instance.player.GetComponent<Player>().isSlime) //슬라임 형태가 아닐때 
-                GameManager.instance.curWaterReserves = 0; //사망
+            if (!GameManager.instance.player.GetComponent<Player>().isSlime
+                && currentWaterReserves >= 3 && collision.transform.position.y <= -2.6f) //남은 물이 3 이상인데 플레이어 위치가 이럴때
+            {
+                GameManager.instance.player.GetComponent<Player>().anim.Play("Die");
+                GameManager.instance.curWaterReserves = 0;
+            }
             else 
             {
                 if ((Input.GetKeyDown(KeyCode.C)))
