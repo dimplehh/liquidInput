6b103beb54fa5adbfab52b7f395757988eb1f9f3
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-08-13 16:50:31 +0900

    챕터선택 2차수정

--- Assets/Resources/Scripts/Save/KeyBoardClick/ChapterLoadKeyBoardClick.cs
+++ Assets/Resources/Scripts/Save/KeyBoardClick/ChapterLoadKeyBoardClick.cs
@@ -6,11 +6,11 @@ public class ChapterLoadKeyBoardClick : MonoBehaviour
 {
     [SerializeField] private ChapterLoad chapterLoad; //챕터별 불러오기
     [SerializeField] private int chapLoadindex = 1; //챕터별 인덱스
-    [SerializeField] private RectTransform selectCheckImage; //선택 확인 이미지
-    [SerializeField] private RectTransform[] selectPos; //선택이미지 위치
     [Header("키보드 이동 위치")]
     [SerializeField] private RectTransform buttonGroup;
     [SerializeField] private RectTransform initButtonGroup; //초기화 위치
+    [Header("챕터 잠금 흑백 이미지")]
+    [SerializeField] private GameObject rockImage;
 
     private void Start()
     {
@@ -23,7 +23,6 @@ public class ChapterLoadKeyBoardClick : MonoBehaviour
     private void OnDisable()
     {
         buttonGroup.position = initButtonGroup.position;
-        selectCheckImage.position = selectPos[0].position;
     }
     private void Update()
     {
@@ -47,15 +46,13 @@ public class ChapterLoadKeyBoardClick : MonoBehaviour
             if (chapLoadindex >= 4)
             {
                 chapLoadindex = 4;
-                selectCheckImage.position = selectPos[3].position;
                 
             }
             else
             {
                 chapLoadindex++;
                 buttonGroup.position = buttonGroup.position - new Vector3(1100, 0, 0);
-                selectCheckImage.position = selectPos[chapLoadindex - 1].position;
-                
+                RockImageCheck();
             }
   
         }
@@ -64,20 +61,31 @@ public class ChapterLoadKeyBoardClick : MonoBehaviour
             if (chapLoadindex <= 1)
             {
                 chapLoadindex = 1;
-                selectCheckImage.position = selectPos[0].position;
             }
             else
             {
                 chapLoadindex--;
                 buttonGroup.position = buttonGroup.position + new Vector3(1100, 0, 0);
-                selectCheckImage.position = selectPos[chapLoadindex - 1].position;
-                
+                //buttonGroup.position = Vector3.Lerp(buttonGroup.position, buttonGroup.position + new Vector3(1100, 0, 0), Time.deltaTime * 0.5f); 
+                RockImageCheck();
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
             chapterLoad.ChapterLoadButton(chapLoadindex);
+        }
+    }
+
+    private void RockImageCheck()
+    {
+        if (StageManager.instance.lastStageIndex < chapLoadindex)
+        {
+            rockImage.SetActive(true);
+        }
+        else
+        {
+            rockImage.SetActive(false);
         }
     }
 }
