5dd96344f4e36755a4da7eb5746676d205f6dd09
HyunHee_LEE <dimplehh@naver.com>
2023-09-01 23:57:49 +0900

    자잘한 오류 몇개 수정

--- Assets/Resources/Scripts/Object/Water/DeepWater.cs
+++ Assets/Resources/Scripts/Object/Water/DeepWater.cs
@@ -28,11 +28,15 @@ public class DeepWater : Water
     {
         if (collision.gameObject.CompareTag("SaveZone"))
         {
-            if (!GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeIdle")) //슬라임 형태가 아닐때 
+            if (!(GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeIdle")
+                || GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeWalk")
+                || GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeRun"))) //슬라임 형태가 아닐때 
             {
                 GameManager.instance.player.GetComponent<Rigidbody2D>().mass = 1.5f;
             }
-            if (!GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeIdle")
+            if (!(GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeIdle")
+                || GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeWalk")
+                || GameManager.instance.player.GetComponent<Player>().anim.GetCurrentAnimatorStateInfo(0).IsName("SlimeRun"))
                 && currentWaterReserves >= 3 && collision.transform.position.y <= deadZone.transform.position.y) //남은 물이 3 이상인데 플레이어 위치가 이럴때
             {
                 GameManager.instance.player.GetComponent<Player>().anim.Play("Die");
