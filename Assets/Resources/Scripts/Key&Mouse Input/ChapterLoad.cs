331a972f8b05eab54527f90cd7b06bc3cd948b16
HyunHee_LEE <dimplehh@naver.com>
2023-09-21 14:21:21 +0900

    서브 일러스트 삽입

--- Assets/Resources/Scripts/Key&Mouse Input/ChapterLoad.cs
+++ Assets/Resources/Scripts/Key&Mouse Input/ChapterLoad.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChapterLoad : MonoBehaviour
 {
     [SerializeField] private GameObject[] lockImage; //챕터 잠금 이미지
-    
+
     private void LockImageCheck()
     {
-
         for (int i = 0; i < StageManager.instance.lastStageIndex - 1; i++)
         {
             lockImage[i].SetActive(false);
         }
     }
-
     private void Start()
     {
         LockImageCheck();
     }
+
     public void ChapterLoadButton(int index)
     {
         if (!StageManager.instance.LastStageCheck(index))
