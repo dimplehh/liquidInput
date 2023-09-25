c9d1a6fd968fbfcab1c809eaa5ad279284553bc0
HyunHee_LEE <dimplehh@naver.com>
2023-09-25 19:49:51 +0900

    1챕 맵꾸 / 배경색 변화 변경

--- Assets/Resources/URP/Lights/LightColorController.cs
+++ Assets/Resources/URP/Lights/LightColorController.cs
@@ -25,7 +25,6 @@ public class LightColorController : MonoBehaviour
 
     private void OnEnable()
     {
-        //time = environMap.transform.position.x / 200;
         GetSetters();
         UpdateSetters();
     }
