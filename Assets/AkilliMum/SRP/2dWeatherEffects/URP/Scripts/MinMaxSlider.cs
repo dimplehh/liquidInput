87360f57f3d680b0fa82ead122575851e7b381f6
HyunHee_LEE <dimplehh@naver.com>
2023-09-26 14:36:10 +0900

    2d weather import / active 상태는 false

---
+++ Assets/AkilliMum/SRP/2dWeatherEffects/URP/Scripts/MinMaxSlider.cs
@@ -0,0 +1,23 @@
+﻿//#define DEBUG_RENDER
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace AkilliMum.SRP.D2WeatherEffects.URP
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class MinMaxSliderAttribute : Attribute
+    {
+        public float MinValue { get; private set; }
+        public float MaxValue { get; private set; }
+
+        public MinMaxSliderAttribute(float minValue, float maxValue)
+        {
+            this.MinValue = minValue;
+            this.MaxValue = maxValue;
+        }
+    }
+}
