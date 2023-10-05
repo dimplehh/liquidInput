fcf9ef25ac336442fd979ba85cb74418b4e4a3b3
HyunHee_LEE <dimplehh@naver.com>
2023-10-05 19:29:47 +0900

    플랜트 몬스터 더미 만듦

---
+++ Assets/Resources/Scripts/Object/PlantMonster.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantMonster : MonoBehaviour
+{
+    [SerializeField] GameObject hitPoint;
+    [SerializeField] float baseY;
+    [SerializeField] float changedY;
+    private float baseX;
+    private float playerPositionX;
+    private float curCheckPlayerTime = 0f;
+    private float maxCheckPlayerTime = 8f;
+    private void Start()
+    {
+        baseX = hitPoint.transform.position.x;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            StartCoroutine("WakeUpPlant");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            StartCoroutine("SleepPlant");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            curCheckPlayerTime += Time.deltaTime;
+            if(curCheckPlayerTime > maxCheckPlayerTime)
+            {
+                playerPositionX = collision.gameObject.transform.position.x;
+                hitPoint.transform.position = new Vector3(playerPositionX, 0, 0);
+                curCheckPlayerTime = 0;
+            }
+            else if ( maxCheckPlayerTime * 2 / 4 <= curCheckPlayerTime && curCheckPlayerTime <=  maxCheckPlayerTime * 3/4)
+            {
+                 hitPoint.transform.position = new Vector3(baseX, changedY, 0);
+            }
+        }
+    }
+
+    IEnumerator WakeUpPlant()
+    {
+        while (hitPoint.transform.position.y < changedY)
+        {
+            hitPoint.transform.Translate(0, 0.1f, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    IEnumerator SleepPlant()
+    {
+        while (hitPoint.transform.position.y > baseY)
+        {
+            hitPoint.transform.Translate(0, -0.1f, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
