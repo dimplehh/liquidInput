1fa306b1b996a45c7ca8f63321c5d120a1dd0d10
HyunHee_LEE <dimplehh@naver.com>
2023-10-06 17:44:53 +0900

    트리거 도달 시 바위함정 실행되는것 구현

--- Assets/Resources/Scripts/Object/StoneHillTrigger.cs
+++ Assets/Resources/Scripts/Object/StoneHillTrigger.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class StoneHillTrigger : MonoBehaviour
 {
     [SerializeField] GameObject stone;
+    [SerializeField] GameObject hill;
     float time = 10f;
     bool playerInTrigger = false;
+    [SerializeField] bool onlyOneTime = false;
     [SerializeField] Vector3 firstStoneExist = new Vector3(-0.43f, 8.35f, 0);
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,6 +34,17 @@ public class StoneHillTrigger : MonoBehaviour
                 stone.transform.localPosition = firstStoneExist;
                 time = 10.0f;
             }
+            if(onlyOneTime)
+            {
+                StartCoroutine("hillTurnOff");
+            }
         }
+    }
+    private IEnumerator hillTurnOff()
+    {
+        yield return new WaitForSeconds(2.0f);
+        if (hill != null)
+            hill.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
     }
 }
