e434f834a3c5d031348e217d297724c53ad9374b
HyunHee_LEE <dimplehh@naver.com>
2023-11-04 18:33:30 +0900

    독극물 1차

--- Assets/Resources/Scripts/Object/RollingStone.cs
+++ Assets/Resources/Scripts/Object/RollingStone.cs
@@ -8,7 +8,7 @@ public class RollingStone : MonoBehaviour
     {
         if (collision.gameObject.tag == "SaveZone")
         {
-            if(this.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude >= 5)
+            if(this.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude >= 2)
             {
                 GameManager.instance.player.GetComponent<Player>().anim.Play("Die");
                 GameManager.instance.OpenGameOver();
