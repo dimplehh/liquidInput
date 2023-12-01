47fce784d0923e103046692d2c43b20246d42360
HyunHee_LEE <dimplehh@naver.com>
2023-12-01 13:15:33 +0900

    크레딧씬 프리뷰 끄기, 진/히든 크레딧 따로 만듦

---
+++ Assets/Resources/Scripts/Scene/CreditScene2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScene2 : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.instance.BgmPlaySound(5);
+    }
+}
