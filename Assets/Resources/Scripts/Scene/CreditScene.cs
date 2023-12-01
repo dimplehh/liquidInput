3a16ea88537555f6c752c096a266e7e934d3791e
HyunHee_LEE <dimplehh@naver.com>
2023-12-01 12:41:54 +0900

    크레딧 씬 구현

---
+++ Assets/Resources/Scripts/Scene/CreditScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScene : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.instance.BgmPlaySound(4);
+    }
+}
